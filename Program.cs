2f645f31d5c3c005cc25a1a5cfcc5989d3b32927
Murat.Can <muratt.can.94@gmail.com>
2023-02-20 12:20:12 +0300

    metots ornekler

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace metots
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int a = 15;
+            int b = 77;
+            int sonuc = toplama(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar metot = new Metotlar();
+            metot.EkranaYazdir(sonuc.ToString());
+        } 
+
+        static int toplama (int sayi1, int sayi2)
+        {
+            return(sayi1+sayi2);
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
